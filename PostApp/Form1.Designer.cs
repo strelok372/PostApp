9e782971b8e0ce4511d3410957f8a0c83e20f690
Vladimir Dozorov <dozorrow@yandex.ru>
2019-05-29 01:17:15 +0300

    first

---
+++ PostApp/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace PostApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label_time_elapsed = new System.Windows.Forms.Label();
+            this.label_display_info = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label_trackbar_value = new System.Windows.Forms.Label();
+            this.label_row_count = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Соединение с БД";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Заполнить таблицу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Загрузить таблицу";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 86);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Очистить таблицу";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 178);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(442, 263);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label_time_elapsed
+            // 
+            this.label_time_elapsed.AutoSize = true;
+            this.label_time_elapsed.Location = new System.Drawing.Point(12, 447);
+            this.label_time_elapsed.Name = "label_time_elapsed";
+            this.label_time_elapsed.Size = new System.Drawing.Size(0, 13);
+            this.label_time_elapsed.TabIndex = 7;
+            // 
+            // label_display_info
+            // 
+            this.label_display_info.AutoSize = true;
+            this.label_display_info.Location = new System.Drawing.Point(40, 8);
+            this.label_display_info.Name = "label_display_info";
+            this.label_display_info.Size = new System.Drawing.Size(158, 13);
+            this.label_display_info.TabIndex = 8;
+            this.label_display_info.Text = "Подключение не установлено";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(142, 86);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Установить B-tree индекс";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(142, 56);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Установить Hash индекс";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10000;
+            this.trackBar1.Location = new System.Drawing.Point(6, 19);
+            this.trackBar1.Maximum = 100000;
+            this.trackBar1.Minimum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(150, 45);
+            this.trackBar1.SmallChange = 10000;
+            this.trackBar1.TabIndex = 11;
+            this.trackBar1.TickFrequency = 10000;
+            this.trackBar1.Value = 1000;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label_trackbar_value
+            // 
+            this.label_trackbar_value.AutoSize = true;
+            this.label_trackbar_value.Location = new System.Drawing.Point(103, 51);
+            this.label_trackbar_value.Name = "label_trackbar_value";
+            this.label_trackbar_value.Size = new System.Drawing.Size(31, 13);
+            this.label_trackbar_value.TabIndex = 12;
+            this.label_trackbar_value.Text = "1000";
+            // 
+            // label_row_count
+            // 
+            this.label_row_count.AutoSize = true;
+            this.label_row_count.Location = new System.Drawing.Point(12, 157);
+            this.label_row_count.Name = "label_row_count";
+            this.label_row_count.Size = new System.Drawing.Size(0, 13);
+            this.label_row_count.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Кол-во записей:";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 128);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(149, 23);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "Выборка из диапазона";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(6, 99);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(149, 23);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "Произвольная выборка";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label_trackbar_value);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Location = new System.Drawing.Point(298, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(161, 163);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Тест";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(6, 70);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(149, 23);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "Произвольная вставка";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(142, 27);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(150, 23);
+            this.button10.TabIndex = 18;
+            this.button10.Text = "Убрать индексы";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(142, 115);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(150, 23);
+            this.button11.TabIndex = 19;
+            this.button11.Text = "Установить GIN индекс";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(19, 12);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(465, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(156, 428);
+            this.richTextBox1.TabIndex = 22;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 469);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label_row_count);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label_display_info);
+            this.Controls.Add(this.label_time_elapsed);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label_time_elapsed;
+        private System.Windows.Forms.Label label_display_info;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label_trackbar_value;
+        private System.Windows.Forms.Label label_row_count;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ PostApp/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mono.Security;
+using Npgsql;
+using System.Diagnostics;
+
+namespace PostApp
+{
+    public partial class Form1 : Form
+    {
+
+        Relabase relabase;
+        public Form1()
+        {
+            InitializeComponent();
+            relabase = new Relabase();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //соединение
+        {
+            EIG(() => relabase.connect());
+            label_display_info.Text = "Соединение установлено";
+            pictureBox1.BackColor = Color.Green;
+        }
+        private void button2_Click(object sender, EventArgs e) //заполнение
+        {
+            EIG(() => label_display_info.Text = "Добавлено: " + relabase.fillTable(100000) + " строк");
+        }
+        private void button3_Click(object sender, EventArgs e) //подгрузка таблицы
+        {
+            try
+            {
+                var w = relabase.getDatasource();
+                dataGridView1.DataSource = w;
+                label_row_count.Text = "Всего строк: " + w.Rows.Count.ToString();
+            }
+            catch (Exception ew)
+            {
+                label_display_info.Text = ew.Message;
+            }
+        }
+        private void button4_Click(object sender, EventArgs e) //очистка 
+        {
+            EIG(() => relabase.clearTable());
+        }
+        private void button5_Click(object sender, EventArgs e) //создание b индекса
+        {
+            EIG(() => relabase.dropHashIndex());
+            EIG(() => relabase.addBTreeIndex());
+        }
+        private void button6_Click(object sender, EventArgs e) //создание hash индекса
+        {
+            EIG(() => relabase.dropBTreeIndex());
+            EIG(() => relabase.addHashIndex());
+        }
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label_trackbar_value.Text = trackBar1.Value.ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EIG(() => new Relabase().connect().dropHashIndex());
+        }
+        private void button9_Click(object sender, EventArgs e) //(тест) случайное заполнение
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            EIG(() => relabase.fillTable(trackBar1.Value));
+            sw.Stop();
+            label_time_elapsed.Text = "s:" + sw.Elapsed.Seconds + ":ms" + sw.Elapsed.Milliseconds / 10;
+        }
+        private void button10_Click(object sender, EventArgs e) //убрать все индексы
+        {
+            EIG(() => relabase.dropBTreeIndex());
+            EIG(() => relabase.dropGinIndex());
+            EIG(() => relabase.dropHashIndex());
+        }
+        private void button8_Click(object sender, EventArgs e) //(тест) случайная выборка
+        {
+            try
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                var w = relabase.randSelect(trackBar1.Value);
+                dataGridView1.DataSource = w;
+                label_row_count.Text = "Всего строк: " + w.Rows.Count.ToString();
+                sw.Stop();
+                label_time_elapsed.Text = "s:" + sw.Elapsed.Seconds + ":ms" + sw.Elapsed.Milliseconds / 10;
+                richTextBox1.AppendText(sw.Elapsed.ToString() + '\n');
+            }
+            catch (Exception ew)
+            {
+                richTextBox1.AppendText(ew.Message + '\n');
+            }
+        }
+        private void button7_Click(object sender, EventArgs e) //(тест) выборка из диапазона
+        {
+            try
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                var w = relabase.diapSelect(trackBar1.Value);
+                dataGridView1.DataSource = w;
+                label_row_count.Text = "Всего строк: " + w.Rows.Count.ToString();
+                sw.Stop();
+                label_time_elapsed.Text = "s:" + sw.Elapsed.Seconds + ":ms" + sw.Elapsed.Milliseconds / 10;
+                richTextBox1.AppendText(sw.Elapsed.ToString() + '\n');
+            }
+            catch (Exception ew)
+            {
+                richTextBox1.AppendText(ew.Message + '\n');
+            }
+        }
+
+        public void EIG(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception ew)
+            {
+                richTextBox1.AppendText(ew.Message + '\n');
+            }
+        } //обработчик исключений
+    }
+
+    public class Relabase
+    {
+        static String dbSettings = "Server=localhost;Port=5432;User Id=postgres;Password=1234;Database=task2;";
+        NpgsqlConnection dbConnection;
+        Random r = new Random();
+        Timer t = new Timer();
+        List<Record> records = new List<Record>();
+
+        public Relabase()
+        {
+            dbConnection = new NpgsqlConnection(dbSettings);
+        }
+        ~Relabase()
+        {
+            dbConnection.Close();
+        }
+        public Relabase connect()
+        {
+            dbConnection.Open();
+            return this;
+        }
+        public int fillTable(int inputCount)
+        {
+            int count = 0;
+            for (int i = 0; i < inputCount; i++)
+            {
+                String ticks = DateTime.Now.ToString();
+                int p = r.Next(1, 4);
+
+                NpgsqlCommand fillCommand = new NpgsqlCommand("INSERT INTO Таблица_1 VALUES(" + p + ", '" + ticks + "');");
+                fillCommand.Connection = dbConnection;
+                count += fillCommand.ExecuteNonQuery();
+            }
+            return count;
+        }
+        public void clearTable()
+        {
+            NpgsqlCommand clearT = new NpgsqlCommand("DELETE FROM Таблица_1");
+            clearT.Connection = dbConnection;
+            clearT.ExecuteNonQuery();
+        }
+        public void getTable()
+        {
+            NpgsqlCommand getT = new NpgsqlCommand("SELECT * FROM Таблица_1");
+            getT.Connection = dbConnection;
+            NpgsqlDataReader t_reader = getT.ExecuteReader();
+            while (t_reader.Read())
+            {
+                records.Add(new Record(
+                t_reader.GetInt32(0),
+                t_reader.GetInt32(1),
+                t_reader.GetString(2)
+                ));
+            }
+        }
+        public DataTable getDatasource()
+        {
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter("SELECT * FROM Таблица_1", dbConnection);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            return dt;
+        }
+        public void addBTreeIndex()
+        {
+            NpgsqlCommand np = new NpgsqlCommand(
+                "CREATE INDEX \"bTree\" ON \"Таблица_1\" USING btree (\"Поле_1\");"
+                );
+            np.Connection = dbConnection;
+            np.ExecuteNonQuery();
+        }
+        public void addHashIndex()
+        {
+            NpgsqlCommand np = new NpgsqlCommand(
+                "CREATE INDEX \"hashTree\" ON \"Таблица_1\" USING hash (\"Поле_1\");"
+                );
+            np.Connection = dbConnection;
+            np.ExecuteNonQuery();
+        }
+        public void dropHashIndex()
+        {
+            NpgsqlCommand np = new NpgsqlCommand("DROP INDEX \"bTree\"");
+            np.Connection = dbConnection;
+            np.ExecuteNonQuery();
+
+
+        }
+        public void dropBTreeIndex()
+        {
+            NpgsqlCommand np = new NpgsqlCommand("DROP INDEX \"hashTree\"");
+            np.Connection = dbConnection;
+            np.ExecuteNonQuery();
+        }
+        public void dropGinIndex()
+        {
+            NpgsqlCommand np = new NpgsqlCommand("DROP INDEX \"ginTree\"");
+            np.Connection = dbConnection;
+            np.ExecuteNonQuery();
+        }
+        public DataTable randSelect(int count)
+        {
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter("SELECT * FROM Таблица_1 ORDER BY random() limit " + count, dbConnection);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            return dt;
+        }
+        public DataTable diapSelect(int count)
+        {
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter("SELECT * FROM Таблица_1 LIMIT " + count + " OFFSET 1000; " + count, dbConnection);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            return dt;
+        }
+
+    }
+    struct Record
+    {
+        int column1;
+        int column2;
+        String column3;
+        public Record(int a, int b, String c)
+        {
+            column1 = a;
+            column2 = b;
+            column3 = c;
+        }
+    }
+}
